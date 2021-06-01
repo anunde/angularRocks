8a4084b3a635bbb7ad1cc2669023b789677151a1
anunde <a.nunez.torre@gmail.com>
2021-06-01 20:33:31 +0200

    Relleno con datos la BBDD

--- src/assets/storage.js
+++ src/assets/storage.js
@@ -1,8 +1,9 @@
 let DB;
 createDB();
 
+
 function createDB() {
-    const createDB = window.indexedDB.open('rockBands', 1);
+    let createDB = window.indexedDB.open('rockBands', 1);
 
     createDB.onerror = function() {
         console.log('Tu navegador no es compatible con IndexedDB');
@@ -10,15 +11,38 @@ function createDB() {
 
     createDB.onsuccess = function() {
         DB = createDB.result;
+
+        fillDB();
     }
 
     createDB.onupgradeneeded = function(e) {
-        const db = e.target.result;
+        let db = e.target.result;
 
-        const objectStore = db.createObjectStore('rockBands', {keyPath: 'id', autoIncrement: true });
+        let objectStore = db.createObjectStore('rockBands', {keyPath: 'id', autoIncrement: true });
 
         objectStore.createIndex('id', 'id', {unique: true});
         objectStore.createIndex('nombre', 'nombre', {unique: true});
         objectStore.createIndex('descripcion', 'descripcion', {unique: false});
+
+    }
+}
+
+function fillDB() {
+
+    let transaction = DB.transaction(['rockBands'], 'readwrite');
+
+    transaction.oncomplete = function(event) {
+        console.log('Transacción Completada');
+    };
+    
+    transaction.onerror = function(event) {
+        console.log('Hubo un error en la transacción')
+    };
+
+    let objectStore = transaction.objectStore('rockBands');
+
+    
+    for(band in musicBands) {
+        objectStore.add(musicBands[band]);
     }
 }
