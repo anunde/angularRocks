d9a1d0bcb9214961e6ec60d692aa93e98557bc6a
anunde <a.nunez.torre@gmail.com>
2021-07-01 21:41:00 +0200

    Despues de la clase con santiago

--- src/app/services/listServices/list.service.ts
+++ src/app/services/listServices/list.service.ts
@@ -1,34 +1,152 @@
-const musicBands = {
-    RollingStones: {
+import { Injectable } from '@angular/core';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListService {
+  public rockBandsStorage = new BehaviorSubject<any>([]);
+  private musicBands: any;
+  private addListSubscription = new Subject<any>();
+  private remoteListSubscription = new Subject<any>();
+  private updateListSubscription = new Subject<any>();
+
+  private subject = new Subject<any>();
+
+  constructor() { 
+    this.musicBands = [
+      {
+        id: '1',
         nombre: 'The Rolling Stones',
         descripcion: 'The Rolling Stones es una banda británica de rock originaria de Londres. La banda se formó en abril de 19622​ por Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Charlie Watts e Ian Stewart.2​ Brian Jones fue despedido en junio de 1969, falleciendo tres semanas después, siendo reemplazado por el guitarrista Mick Taylor, que dejaría el grupo en 1975 y sería a su vez reemplazado por Ronnie Wood. Con el retiro de Bill Wyman en 1993 se incluyó al bajista Darryl Jones que, aunque toca con la banda desde la grabación del álbum Voodoo Lounge en 1994, no es un miembro oficial. Están considerados como una de las más grandes e influyentes bandas de toda la historia del rock.'
-    },
-    LedZeppelin: {
+      },
+      {
+        id: '2',
         nombre: 'Led Zeppelin',
         descripcion: 'Led Zeppelin fue un grupo británico de hard rock fundado en Londres en 1968 por el guitarrista Jimmy Page, quien había pertenecido a The Yardbirds. La banda estuvo integrada por John Paul Jones como bajista y teclista, el vocalista Robert Plant y John Bonham a la batería (que había coincidido con Plant en The Band of Joy). Led Zeppelin presentó elementos de un amplio espectro de influencias, como el blues, el rock and roll, el soul, la música celta, la música india, el folk, y el country, siendo uno de los grupos seminales para el surgimiento del heavy metal.'
-    },   
-    Queen: {
+      },   
+      {
+        id: '3',
         nombre: 'Queen',
         descripcion: 'Queen es una banda británica de rock formada en 1970 en Londres por el cantante y pianista Freddie Mercury, el guitarrista Brian May, el baterista Roger Taylor y el bajista John Deacon. Si bien el grupo ha presentado bajas de dos de sus miembros (Mercury, fallecido en 1991, y Deacon, retirado en 1997), los integrantes restantes, Brian May y Roger Taylor, continúan trabajando bajo el nombre Queen, por lo que la banda aún se considera activa.'
-    },
-    PinkFloyd: {
+      },
+      {
+        id: '4',
         nombre: 'Pink Floyd',
         descripcion: 'Pink Floyd fue una banda de rock británica, fundada en Londres en 1965. Es considerada un ícono cultural del siglo XX y una de las bandas más influyentes y aclamadas en la historia de la música, obtuvieron gran popularidad gracias a su música psicodélica que evolucionó hacia el rock progresivo y rock sinfónico con el paso del tiempo. Es conocida por sus canciones de alto contenido filosófico, la experimentación sónica, las innovadoras portadas de sus discos y sus elaborados espectáculos en vivo. Sus ventas sobrepasan los 350 millones de álbumes vendidos en todo el mundo,4​5​ 97,5 millones de ellos solamente en los Estados Unidos.​​'
-    },
-    DeepPurple: {
+      },
+      {
+        id: '5',
         nombre: 'Deep Purple',
         descripcion: 'Deep Purple es una banda británica de hard rock formada en 1968 en Hertford, Reino Unido. Está considerada como una de las pioneras de dicho subgénero, y los pioneros del heavy metal junto a Led Zeppelin y Black Sabbath. Su música ha incorporado elementos del rock progresivo, rock sinfónico, rock psicodélico, blues rock, funk rock, britpop y la música clásica. Deep Purple ha vendido más de 150 millones de discos en todo el mundo.​​'
-    },
-    ACDC: {
+      },
+      {
+        id: '6',
         nombre: 'AC/DC',
         descripcion: 'AC/DC es una banda de hard rock británica-australiana, formada en 1973 en Australia por los hermanos escoceses Malcolm y Angus Young. En 1974 incursionaron por primera vez fuera de los conciertos en bandas de punk rock locales que ofrecían un contrapunto sonoro a las ampulosidades y la fastuosidad de las bandas que triunfaron en el mercado de la época.16​17​18​ Ese mismo año se trasladaron Glasgow de donde procedían Desembarcaron en pleno auge del punk rock, lo que contribuyó a que en poco tiempo obtuvieron una enorme aceptación del público, ocupando inmediatamente los primeros puestos en ventas.'
-    },
-    Ramones: {
+      },
+      {
+        id: '7',
         nombre: 'The Ramones',
         descripcion: 'Ramones fue una banda de punk formada en Forest Hills, en el distrito de Queens (Nueva York, Estados Unidos) en 1974, y disuelta veintidós años más tarde, en 1996. Pioneros y líderes del naciente punk, cimentaron las bases de este género musical con composiciones simples, minimalistas, repetitivas y letras muy simples o incluso sin sentido, en clara oposición a la pomposidad y la fastuosidad de las bandas que triunfaban en el mercado de los años 1970:3​4​ con sus largos solos de guitarra, las complejas canciones de rock progresivo y sus enigmáticas letras.5​ Su sonido se caracteriza por ser rápido y directo, con influencias del rockabilly de los años 1950, el surf rock, The Beatles, The Who, The Velvet Underground, las bandas de chicas de los años 1960 como The Shangri-Las y el garage protopunk de MC5 y The Stooges.6​'
-    },
-    Kiss: {
+      },
+      {
+        id: '8',
         nombre: 'Kiss',
         descripcion: 'Kiss (estilizado KISS) es una banda estadounidense de rock formada en Nueva York en enero de 1973 por el bajista Gene Simmons y el guitarrista Paul Stanley, a los que más tarde se unirían el batería Peter Criss y el guitarrista Ace Frehley.2​ Conocidos por su maquillaje facial y sus extravagantes trajes, el grupo se dio a conocer al público a mediados de los años 1970 gracias a sus actuaciones en directo, en las que incluían pirotecnia, llamaradas, cañones, baterías elevadoras, guitarras con humo y sangre falsa. Si se tienen en cuenta los álbumes en solitario de 1978, Kiss ha conseguido treinta discos de oro de la RIAA hacia 2015 y se la considera la banda estadounidense que ha recibido más de estas certificaciones.nota 1​4​ El conjunto ha vendido más de 80 millones de álbumes en todo el mundo y veinticinco millones solo en los Estados Unidos.5​ A pesar de no haber alcanzado la primera posición del Billboard 200, ha situado veintiséis de sus trabajos entre los cuarenta primeros puestos.6​ Desde su formación, los miembros de Kiss asumieron personalidades acordes con los maquillajes y vestimenta que llevaban, así Simmons asumió el rol de «Demon», Stanley el de «Starchild», Criss el de «Catman» y Frehley el de «Space Ace» o «Spaceman»​​'
-    }    
+      }  
+      ];
+
+      // this.addListSubscription.subscribe(data => {
+      //   this.rockBandsStorage = JSON.parse(localStorage.getItem("rockBands")!);
+      //   this.rockBandsStorage.push(data);
+      //   console.log(this.rockBandsStorage);
+      //   this.saveList();
+      // })
+
+      // this.remoteListSubscription.subscribe(band => {
+      //   for(let i = 0; i<this.rockBandsStorage.length; i++) {
+      //     if(band == this.rockBandsStorage[i]) {
+      //       this.rockBandsStorage.splice(i, 1);
+      //       this.saveList();
+      //     }
+      //   } 
+      // })
+
+      // this.updateListSubscription.subscribe(band => {
+      //   for(let i = 0; i<this.rockBandsStorage.length; i++) {
+      //     if(band.id === this.rockBandsStorage[i].id) {
+      //       this.rockBandsStorage[i] = band;
+      //       this.saveList();
+      //     }
+      //   }
+      // })
+
+      this.rockBandsStorage.next(JSON.parse(localStorage.getItem("rockBands")!));
+      console.log(this.rockBandsStorage, window.location.href);
+
+  }
+
+  public initLocalStorage() {
+    localStorage.setItem('rockBands' , JSON.stringify(this.musicBands));
+    this.rockBandsStorage.next(JSON.parse(localStorage.getItem("rockBands")!));
+  }
+
+  public saveList() {
+    localStorage.setItem('rockBands' , JSON.stringify(this.rockBandsStorage.value));
+  }
+
+
+  public addBand(data: any) {
+    this.rockBandsStorage.next([...this.rockBandsStorage.value, data]);
+    this.saveList();
+  }
+
+  public getAllBand() {
+    return this.rockBandsStorage;
+  }
+
+  public getOneBand(id: string) {
+    let banda = this.rockBandsStorage.value.find((el:any)=>{
+      return el.id == id
+    })
+    console.log(this.rockBandsStorage.value);
+    return banda;
+  } 
+
+  public updateBand(band: any) {
+    let updatedLista = this.rockBandsStorage.value.map((rockBand: any) => {
+      if(rockBand.id === band.id) {
+        return band;
+      }
+
+      return rockBand;
+    })
+    this.rockBandsStorage.next(updatedLista);
+    this.saveList();
+  }
+
+  public deleteBand(band: any) {
+    let updatedLista = this.rockBandsStorage.value.filter((rockBand: any) => {
+      return rockBand.id !== band.id
+    })
+    this.rockBandsStorage.next(updatedLista);
+    this.saveList();
+  }
+
+  public sendDetails(id:string)
+  {
+    let banda = this.rockBandsStorage.value.filter((el:any)=>{
+      return el.id == id
+    })
+
+    this.subject.next({band: banda});
+  }
+
+  public getDetails():Observable<any>
+  {
+    console.log('devolviendo')
+    return this.subject.asObservable()
+  }
+
 }
