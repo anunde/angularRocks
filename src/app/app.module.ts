63a667efc1e6509fef116b574295d86764046dc1
anunde <a.nunez.torre@gmail.com>
2021-07-01 21:41:00 +0200

    Termino el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,27 @@
 import { NgModule } from '@angular/core';
-import { routing, appRoutingProviders } from './app.routing';
+import { AppRoutingModule } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
-import { FormularioComponent } from './formulario/formulario.component';
+import {BandComponent} from './band/band.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    BandComponent
   ],
   imports: [
-    routing,
+    AppRoutingModule,
     BrowserModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule
+    CommonModule
   ],
   providers: [
-    appRoutingProviders,
+
   ],
   bootstrap: [AppComponent]
 })
